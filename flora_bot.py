53373b65f3f3ae0d986366f1bfc46bdb1ec15865
omaraly22 <112936089+omaraly22@users.noreply.github.com>
2023-05-28 12:32:14 +0300

    Updated the bot

--- flora_bot.py
+++ flora_bot.py
@@ -7,9 +7,8 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 import tempfile
 from langchain import PromptTemplate
-import os
 
-openai_api_key = os.environ.get('OPENAI_API_KEY')
+openai_api_key = st.secrets['OPENAI_API_KEY']
 
 product_format = """
 
@@ -59,12 +58,12 @@ question: {question}
 """
 
 
-uploaded_file = st.sidebar.file_uploader("upload", type="csv")
+# uploaded_file = st.sidebar.file_uploader("upload", type="csv")
 
-if uploaded_file :
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
+# if uploaded_file :
+#     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+#         tmp_file.write(uploaded_file.getvalue())
+#         tmp_file_path = tmp_file.name
 
     # loader = AirbyteJSONLoader(file_path=tmp_file_path, encoding="utf-8")
         
@@ -73,65 +72,65 @@ if uploaded_file :
     #     jq_schema='algoliaProducts[]')
 
 
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
+loader = CSVLoader(file_path='./floward_sample_data.csv', encoding="utf-8")
 
-    data = loader.load()
+data = loader.load()
 
-    # data = pd.read_json(tmp_file_path)
+# data = pd.read_json(tmp_file_path)
     
-    st.write(data)
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    vectors = FAISS.from_documents(data, embeddings)
+st.write(data)
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+vectors = FAISS.from_documents(data, embeddings)
 
 
-    custom_llm = ChatOpenAI(temperature=0,
+custom_llm = ChatOpenAI(temperature=0,
                     model_name='gpt-3.5-turbo',
                     openai_api_key=openai_api_key)
     
-    system_message_prompt = PromptTemplate(template=system_message, input_variables=['context', 'question'])
-    
-    def conversational_chat(query):
-    
-        retriever = vectors.as_retriever()
+system_message_prompt = PromptTemplate(template=system_message, input_variables=['context', 'question'])
 
-        chain = ConversationalRetrievalChain.from_llm(llm = custom_llm,
+def conversational_chat(query):
+    
+    retriever = vectors.as_retriever()
+
+    chain = ConversationalRetrievalChain.from_llm(llm = custom_llm,
                                                     retriever=retriever, 
                                                     combine_docs_chain_kwargs={'prompt': system_message_prompt})
 
             
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
+    result = chain({"question": query, "chat_history": st.session_state['history']})
+    st.session_state['history'].append((query, result["answer"]))
         
-        return result["answer"]
+    return result["answer"]
     
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
 
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello! I am Flora, an Assistant Bot from Floward. How may I assist you today? 🤗"]
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["Hello! I am Flora, an Assistant Bot from Floward. How may I assist you today? 🤗"]
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
+if 'past' not in st.session_state:
+    st.session_state['past'] = ["Hey ! 👋"]
         
-    #container for the chat history
-    response_container = st.container()
-    #container for the user's text input
-    container = st.container()
+#container for the chat history
+response_container = st.container()
+#container for the user's text input
+container = st.container()
 
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
             
-            user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
+        user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
+        submit_button = st.form_submit_button(label='Send')
             
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
+    if submit_button and user_input:
+        output = conversational_chat(user_input)
             
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
 
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
